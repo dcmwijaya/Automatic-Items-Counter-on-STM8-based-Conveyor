649f3cf8323605d677c8e9178a64a0dd0d570581
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-03-04 15:06:50 +0700

    Update auto_counter.ino

--- Src/auto_counter.ino
+++ Src/auto_counter.ino
@@ -6,7 +6,7 @@
 int counter = 0;
 int currentState = 0;
 int previousState = 0;
-int ldrStatus;
+int ldrValue;
 
 void setup() {
   Serial_begin(9600);
@@ -16,18 +16,22 @@ void setup() {
 }
  
 void loop() {
-  int ldrStatus = analogRead(ldrPin);
+  int ldrValue = analogRead(ldrPin);
+  Serial_print_s("\n\nLDR Value\t: ");
+  Serial_print_i(ldrValue);
+  Serial_print_s("\nTotal items\t: ");
 
-  if(ldrStatus <= 700){ currentState = 1; }
+  if(ldrValue <= 721){ currentState = 1; }
   else{ currentState = 0; } 
-  delay(200);
+  delay(1000);
 
   if(currentState != previousState){
     if(currentState == 1){
       counter = counter + 1;
-      Serial_print_s("Hitung Barang yang lewat: ");
-      Serial_print_s(counter);
-      Serial_print_s("\n");
+      Serial_print_i(counter);
     }
+  }
+  else {
+    Serial_print_i(counter);
   }
 }
