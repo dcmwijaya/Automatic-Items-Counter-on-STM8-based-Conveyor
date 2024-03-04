cc283ace400091ea60ea8f8945dc6e63d051f097
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-03-04 19:53:13 +0700

    Update auto_counter.ino

--- Src/auto_counter.ino
+++ Src/auto_counter.ino
@@ -1,19 +1,27 @@
 #define ldrPin PD2
 #define laserPin PD3
+#define pbPin PD4
 
 int counter = 0;
 int currentState = 0;
 int previousState = 0;
-int ldr;
+int ldr, pushbutton;
 
 void setup() {
   Serial_begin(9600);
-  pinMode(ldrPin, INPUT);
-  pinMode(laserPin, OUTPUT);
-  digitalWrite(laserPin, HIGH);
+  pinMode(pbPin,INPUT);
+  pinMode(ldrPin,INPUT);
+  pinMode(laserPin,OUTPUT);
+  digitalWrite(laserPin,HIGH);
 }
 
 void loop() {
+  sensor();
+  display();
+  reset();
+}
+
+void sensor(){
   ldr = digitalRead(ldrPin);
   if(ldr == LOW){ currentState = 0; }
   if(ldr == HIGH){ currentState = 1; } 
@@ -21,4 +29,13 @@ void loop() {
   if(currentState == 0 && currentState == previousState){ Serial_print_i(counter-1); }
   else{ counter = counter + 1; Serial_print_i(counter-1); }
   delay(1000);
+}
+
+void display(){
+  
+}
+
+void reset(){
+  pushbutton = digitalRead(pbPin); 
+  if(pushbutton == HIGH){ currentState = 0; };
 }
