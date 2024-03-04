51edc34052e5a8775df4949e775b894bdfc471af
Devan C. M. Wijaya, S.Kom <54527592+devancakra@users.noreply.github.com>
2024-03-05 01:02:13 +0700

    Update auto_counter.ino

--- Src/auto_counter.ino
+++ Src/auto_counter.ino
@@ -1,6 +1,9 @@
+#include <LiquidCrystal_I2C.h>
+
 #define ldrPin PD2
 #define laserPin PD3
 #define pbPin PD4
+#define LCD_ADDR 0x27
 
 int counter = 0;
 int currentState = 0;
@@ -8,18 +11,26 @@ int previousState = 0;
 int ldr = 0; 
 bool pushbutton = false;
 
+LiquidCrystal_I2C(lcd, 0x27, 16, 2);
+
 void setup() {
   Serial_begin(9600);
-  pinMode(pbPin,INPUT);
-  pinMode(ldrPin,INPUT);
-  pinMode(laserPin,OUTPUT);
-  digitalWrite(laserPin,HIGH);
+  device_init();
 }
 
 void loop() {
   sensor();
   reset();
   display();
+}
+
+void device_init(){
+  pinMode(pbPin,INPUT);
+  pinMode(ldrPin,INPUT);
+  pinMode(laserPin,OUTPUT);
+  digitalWrite(laserPin,HIGH);
+  lcd_begin();
+  lcd_backlight();
 }
 
 void sensor(){
@@ -41,5 +52,8 @@ void reset(){
 }
 
 void display(){
-  
+  lcd_setCursor(2, 0);
+  lcd_print_s("Total items:");
+  lcd_setCursor(7, 1);
+  lcd_print_i(counter); 
 }
